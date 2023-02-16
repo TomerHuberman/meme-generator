b2febb1ef6cff51e3fa8947499fbf244aebfbb08
Tomer Huberman <tomerh1999@gmail.com>
2023-02-16 12:15:48 +0200

    with touch

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -3,6 +3,7 @@
 let gElCanvas
 let gCtx
 let gStartPos
+const TOUCH_EVS = ['touchstart', 'touchmove', 'touchend']
 
 function onInit() {
     gElCanvas = document.querySelector('canvas')
@@ -93,17 +94,17 @@ function getEvPos(ev) {
         y: ev.offsetY,
     }
     // Check if its a touch ev
-    // if (TOUCH_EVS.includes(ev.type)) {
-    //     //soo we will not trigger the mouse ev
-    //     ev.preventDefault()
-    //     //Gets the first touch point
-    //     ev = ev.changedTouches[0]
-    //     //Calc the right pos according to the touch screen
-    //     pos = {
-    //         x: ev.pageX - ev.target.offsetLeft - ev.target.clientLeft,
-    //         y: ev.pageY - ev.target.offsetTop - ev.target.clientTop,
-    //     }
-    // }
+    if (TOUCH_EVS.includes(ev.type)) {
+        //soo we will not trigger the mouse ev
+        ev.preventDefault()
+        //Gets the first touch point
+        ev = ev.changedTouches[0]
+        //Calc the right pos according to the touch screen
+        pos = {
+            x: ev.pageX - ev.target.offsetLeft - ev.target.clientLeft,
+            y: ev.pageY - ev.target.offsetTop - ev.target.clientTop,
+        }
+    }
     return pos
 }
 
